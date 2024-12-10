ba6187b8be4b832dcd7c0767e378e9ec1a45ff44
Азирет Бейшенов <beyshenov11@icloud.com>
2024-12-10 22:07:08 +0600

    add hw3

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import PokemonsPage from "./page/pokemonsPage/PokemonsPage";
 function App() {
     return (
         <div className="App">
-            {/*<ChangeTitle/>*/}
-            {/*  <UsersPage/>*/}
-            {/*<SearchBar/>*/}
-            {/*<UserList/>*/}
+
             <PokemonsPage/>
         </div>
     );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,8 @@
 import {combineReducers} from "redux";
-import usersReducer from "./usersReducer";
 import {pokemonsReducer} from "./pokemonsReducer";
 
 
 export const rootReducer = combineReducers({
-    usersReducer,
+
     pokemonsReducer
 })
